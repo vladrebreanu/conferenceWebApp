0f4469e8f2cfa3311887ced5bfba28fee303ae3f
Vlad Rebreanu <vlad.rebreanu@gmail.com>
2021-01-28 18:43:54 +0200

    msg

--- conferenceWebApp/src/main/java/com/spring/conferenceWebApp/ConferenceWebAppApplication.java
+++ conferenceWebApp/src/main/java/com/spring/conferenceWebApp/ConferenceWebAppApplication.java
@@ -2,12 +2,32 @@ package com.spring.conferenceWebApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//@SpringBootApplication
+//public class ConferenceWebAppApplication extends SpringBootServletInitializer {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ConferenceWebAppApplication.class, args);
+//	}
+//
+//}
 
 @SpringBootApplication
-public class ConferenceWebAppApplication {
+public class ConferenceWebAppApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ConferenceWebAppApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConferenceWebAppApplication.class, args);
 	}
 
+//	@RequestMapping(value = "/")
+//	public String hello() {
+//		return "Hello World from Tomcat";
+//	}
 }
--- conferenceWebApp/src/main/java/com/spring/conferenceWebApp/config/PersistenceConfiguration.java
+++ conferenceWebApp/src/main/java/com/spring/conferenceWebApp/config/PersistenceConfiguration.java
@@ -1,9 +1,10 @@
 package com.spring.conferenceWebApp.config;
 
-import javax.sql.DataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration
